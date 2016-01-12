46d1e1e20f5a6121808fdc07ae6b0ed1c3de63ae
aostrikov <aostrikov@technoserv.com>
2016-01-12 18:53:15 +0300

    more stuff

--- cooking/pandas_magic.py
+++ cooking/pandas_magic.py
@@ -8,12 +8,14 @@ import json
 import pandas as pd
 
 from pandas.io.json import json_normalize
+
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
 
 def underscope(line):
     return '_'.join(line.split())
-    
+   
 
 # Read data 
 
@@ -22,7 +24,17 @@ with open('data/train_small.json') as input:
     data = json.load(input)
 
 df = json_normalize(data, 'ingredients', ['id', 'cuisine'])
-df.rename(columns = { 0: 'ingredient'}, inplace=True)
+df.rename(columns = {0: 'ingredient'}, inplace=True)
+
+
+# Add cuisine id
+cuisine_set = set(df.groupby('cuisine').count().index.values)
+cuisine_id_map = {}
+
+for (i, value) in enumerate(cuisine_set):
+    cuisine_id_map[value] = i
+
+df['cuisine_id'] = df.cuisine.apply(lambda x: cuisine_id_map[x])
 
 
 # Remove singles
@@ -30,9 +42,11 @@ group_by_ing = df.groupby('ingredient').count()
 single_ings = group_by_ing[group_by_ing.id == 1].index.values
 without_singles = df[~df.ingredient.isin(single_ings)]
 
+
 # Replace spaces with underscopes
 without_singles.loc[:,'ingredient'] = without_singles.ingredient.apply(underscope)
 df = without_singles
+
 
 # Add vectorized column from words
 vectorizer = CountVectorizer(analyzer="word", max_features=5500)
@@ -40,6 +54,12 @@ vectors = vectorizer.fit_transform(df.ingredient).toarray()
 
 df = df.reset_index(drop=True)
 df['vec'] = pd.Series(list(vectors))
+
+
+
+# Train model
+forest = RandomForestClassifier(n_estimators=10)
+forest.fit(df.vec.values, df.cuisine_id)
 
 
 
