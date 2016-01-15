bd213daf19d773f6dbd7bb608ec55003f8daa311
aostrikov <aostrikov@technoserv.com>
2016-01-15 18:07:03 +0300

    added ML lib

--- sklearn-practice/iris_exploration.py
+++ sklearn-practice/iris_exploration.py
@@ -6,9 +6,17 @@ Created on Fri Jan 15 11:43:24 2016
 """
 
 import pandas as pd
-import seaborn as sns;
+
+import matplotlib.pyplot as plt
+
+from sklearn.cross_validation import train_test_split
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 from sklearn.datasets import load_iris
+from sklearn import svm
+
 
 data = load_iris()
 
@@ -17,9 +25,39 @@ data['data']
 data['target']
 data['target_names']
 
-columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+# ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 
-df = pd.DataFrame(data['data'], columns=columns)
+# iris_type = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+
+df = pd.DataFrame(data['data'], columns = ['sl', 'sw', 'pl', 'pw'])
+df['type'] = data['target']
+
+train, test = train_test_split(df, test_size = 0.2)
+
+X = train[['sl', 'sw', 'pl', 'pw']]
+#X = train[['sl', 'sw']]
+y = train['type']
+
+svc = svm.SVC(kernel='rbf', C=1.0)
+svc.fit(X, y)
+
+X_test = test[['sl', 'sw', 'pl', 'pw']]
+#X_test = test[['sl', 'sw', 'pl', 'pw']]
+
+predicted = svc.predict(X_test)
+actual = test['type']
+
+print confusion_matrix(predicted, actual)
+
+#plt.figure(figsize=(8, 8))
+
+#plt.subplots_adjust(bottom=.05, top=.9, left=.05, right=.95)
+plt.scatter(train['sl'], train['sw'], marker='x')
+plt.scatter(test['sl'], test['sw'], marker='o')
+#plt.show()
+
+
+
 
 
 
